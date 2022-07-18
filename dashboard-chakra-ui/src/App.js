489c03d1fd42033f06890aa77c22c1f6650411fc
Mark_Leo <ratrateroo@gmail.com>
2022-07-18 23:16:16 +0800

    added outlet and auth layout to the app

--- dashboard-chakra-ui/src/App.js
+++ dashboard-chakra-ui/src/App.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 import { Routes, Outlet, Route } from "react-router-dom";
-
+import AuthLayout from "./layouts/AuthLayout";
 function App() {
   return (
     <Fragment>
       <Routes>
         <Route path="/auth" element={<AuthLayout />} />
       </Routes>
+      <Outlet />
     </Fragment>
   );
 }
